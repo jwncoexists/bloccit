c296240bda456df09e970300af966d1506711444
Jennifer Nelson <jenniferwnelson@sbcglobal.net>
2013-09-29 16:50:48 -0700

    First commit and README update

---
+++ README.md
@@ -0,0 +1,4 @@
+# bloccit
+
+A Reddit-style application to demonstration basic web programming skills.
+This was eveloped as part of BLOC's Web Development apprenticship (see [Bloc](http://bloc.io))
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = 'e77daf36752aed52629fb27f4dc2132dfa4cbb5de8124f7d8c87a1950668fe1baf09e4fd145f6a9de98f9625e22e87e2aaaa1b52c59b55ae85c7bb678312bf9a'
